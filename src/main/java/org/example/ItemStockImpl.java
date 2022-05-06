43027273de4796c35d3af46e790880f3e29e47bb
sugimomoto <sugimomoto@gmail.com>
2022-05-06 11:05:16 +0900

    Added ItemStock grouping tests

---
+++ src/main/java/org/example/ItemStockImpl.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class ItemStockImpl implements ItemStock {
+
+    @Override
+    public void add(String item, int num) {
+
+    }
+
+    @Override
+    public int size(String item) {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(String name) {
+        return false;
+    }
+}

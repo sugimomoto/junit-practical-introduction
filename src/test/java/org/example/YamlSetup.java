d730e96a1f85a4404cb17a07f1b0e6587b73aa07
sugimomoto <sugimomoto@gmail.com>
2022-05-08 08:57:40 +0900

    Added Yaml

--- src/test/java/org/example/YamlSetup.java
+++ src/test/java/org/example/YamlSetup.java
@@ -3,6 +3,9 @@ package org.example;
 import org.junit.Test;
 import org.yaml.snakeyaml.Yaml;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -16,5 +19,31 @@ public class YamlSetup {
         Book book = (Book)new Yaml().load(getClass().getResourceAsStream("book.yaml"));
 
         assertThat(book.getTitle(),is("Refactoring"));
+    }
+
+    @Test
+    public void mapYaml() throws Exception{
+        Map sample = new HashMap<String,String>();
+        sample.put("first","hello");
+        sample.put("last","world");
+
+        Yaml yaml = new Yaml();
+        String actual = yaml.dump(sample);
+
+        assertThat(actual,is("first: hello\n\rlast: world"));
+
+
+    }
+
+    @Test
+    public void generateYaml() throws Exception{
+        User user = new User();
+        user.setName("Hello Yaml");
+        user.setAge(23);
+
+        Yaml yaml = new Yaml();
+        assertThat(yaml.dump(user),is("name: HelloYaml\n\rage: 23"));
+
+
     }
 }

face3ec9c8bbf4dfcc5dd051ede3b329f864dd05
sugimomoto <sugimomoto@gmail.com>
2022-05-14 09:39:16 +0900

    Added　rule tests

---
+++ src/test/java/org/example/RuleTest.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(Enclosed.class)
+public class RuleTest {
+
+    public static class TimeOutRuleTest{
+        @Rule
+        public Timeout timeout = new Timeout(1000);
+
+        @Test
+        public void 時間がかかるテスト(){
+            // 省略
+        }
+    }
+
+    public static class TemporaryFolderExcampleTest{
+        @Rule
+        public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+        @Test
+        public void mkDefaultFilesで2つのファイルが作成される() throws Exception{
+            File folder = new File(temporaryFolder.getRoot() + "/sample");
+            folder.mkdir();
+
+            assertThat(folder.list().length,is(1));
+
+        }
+    }
+
+
+}

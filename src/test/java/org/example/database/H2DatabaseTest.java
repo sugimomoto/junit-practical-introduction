2b2722c9d6cf7ea0587566e0e4d73b7c5953d097
sugimomoto <sugimomoto@gmail.com>
2022-06-10 07:32:41 +0900

    Added　H2DataBase Test

---
+++ src/test/java/org/example/database/H2DatabaseTest.java
@@ -0,0 +1,37 @@
+package org.example.database;
+
+import org.h2.util.JdbcUtils;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class H2DatabaseTest {
+
+    @Rule
+    public H2DatabaseServer server = new H2DatabaseServer("h2","db","ut");
+
+    @Test
+    public void ReadTest() throws SQLException, ClassNotFoundException {
+        System.out.println( "Hello World!" );
+
+        Class.forName ("org.h2.Driver");
+        Connection conn = DriverManager.getConnection ("jdbc:h2:~/db", "sa","");
+
+        Statement st = conn.createStatement();
+
+        st.execute("DROP TABLE IF EXISTS TEST;");
+        st.execute("CREATE TABLE TEST(ID INT PRIMARY KEY, NAME VARCHAR(255));");
+        st.execute("INSERT INTO TEST VALUES(1, 'Hello');");
+        st.execute("INSERT INTO TEST VALUES(2, 'World');");
+
+        ResultSet rs = st.executeQuery("SELECT * FROM TEST");
+
+        while(rs.next()){
+            System.out.println(rs.getString("ID"));
+            System.out.println(rs.getString("NAME"));
+        }
+
+    }
+}

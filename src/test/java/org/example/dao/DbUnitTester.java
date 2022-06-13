6e50a718ed225d54aafcfe1ae2c8afaa7507365e
sugimomoto <sugimomoto@gmail.com>
2022-06-14 07:53:01 +0900

    Add DbUnitTester

--- src/test/java/org/example/dao/DbUnitTester.java
+++ src/test/java/org/example/dao/DbUnitTester.java
@@ -2,16 +2,80 @@ package org.example.dao;
 
 
 import org.dbunit.AbstractDatabaseTester;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.ext.h2.H2DataTypeFactory;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
-public class DbUnitTester extends AbstractDatabaseTester implements TestRule {
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public abstract class DbUnitTester extends AbstractDatabaseTester implements TestRule {
+
+    private final String connectionUrl;
+    private final String username;
+    private final String password;
+
+    public DbUnitTester(String driverClass, String connectionUrl){
+        this(driverClass,connectionUrl,null,null);
+    }
+
+    public DbUnitTester(String driverClass, String connectionUrl, String username, String password) {
+        this(driverClass,connectionUrl,username,password, null);
+    }
+
+    public DbUnitTester(String driverClass, String connectionUrl, String username, String password, String schema)  {
+        super(schema);
+        this.connectionUrl = connectionUrl;
+        this.username = username;
+        this.password = password;
+
+        assertNotNullNorEmpty("driverClass",driverClass);
+
+        try{
+            // JDBC ドライバのロード
+            Class.forName(driverClass);
+
+        }catch (ClassNotFoundException ex){
+            throw new AssertionError(ex);
+        }
+    }
+
     @Override
     public IDatabaseConnection getConnection() throws Exception {
-        return null;
+        Connection conn = null;
+        if(username == null & password == null){
+            conn = DriverManager.getConnection(connectionUrl);
+        }else{
+            conn = DriverManager.getConnection(connectionUrl,username,password);
+        }
+        DatabaseConnection dbConnection = new DatabaseConnection(conn, getSchema());
+        DatabaseConfig config = dbConnection.getConfig();
+        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY,new H2DataTypeFactory());
+
+        return dbConnection;
     }
+
+    protected void executeQuery(String sql) throws Exception{
+        Connection conn = getConnection().getConnection();
+        conn.createStatement().execute(sql);
+        conn.commit();
+        conn.close();
+    }
+
+    protected void before() throws Exception{
+
+    }
+
+    protected void after() throws Exception{
+
+    }
+
+    abstract protected IDataSet createDataSet() throws Exception;
 
     @Override
     public Statement apply(Statement base, Description description) {

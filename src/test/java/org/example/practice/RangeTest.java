2ac009917bb19a0dac894ca73d826f7be5ccf75f
Kazuya Sugimoto <sugimomoto@gmail.com>
2022-08-17 07:41:47 +0900

    Fixed RangeTest

--- src/test/java/org/example/practice/RangeTest.java
+++ src/test/java/org/example/practice/RangeTest.java
@@ -1,29 +1,58 @@
 package org.example.practice;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.ExecutionException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(Enclosed.class)
 public class RangeTest {
 
-    @Test
-    public void min0_0かつmax10_5のRangeの時(){
-        Range range = new Range(0.0, 10.5);
+    @RunWith(Enclosed.class)
+    public static class Rangeが0から10_5で{
 
-        assertThat(range.contains(-0.1),is(false));
-        assertThat(range.contains(0.0),is(true));
-        assertThat(range.contains(10.5),is(true));
-        assertThat(range.contains(10.6),is(false));
-    }
+        @RunWith(Theories.class)
+        public static class かつ範囲外の場合{
+            Range sut;
 
-    @Test
-    public void min_5_1かつmax5_1のRangeの時(){
-        Range range = new Range(-5.1, 5.1);
+            @Before
+            public void setUp(){
+                sut = new Range(0d,10.5);
+            }
 
-        assertThat(range.contains(-5.2),is(false));
-        assertThat(range.contains(-5.1),is(true));
-        assertThat(range.contains(5.1),is(true));
-        assertThat(range.contains(5.2),is(false));
+            @DataPoints
+            public static double[] VALUES = {-0.1, 10.6};
+
+            @Theory
+            public void containsはfalseを返す(double value) throws Exception{
+                assertThat("value="+value,sut.contains(value),is(false));
+            }
+        }
+
+        @RunWith(Theories.class)
+        public static class かつ範囲内の場合{
+            Range sut;
+
+            @Before
+            public void setUp(){
+                sut = new Range(0d,10.5);
+            }
+
+            @DataPoints
+            public static double[] VALUES = {0.0, 10.5};
+
+            @Theory
+            public void containsはtrueを返す(double value) throws Exception{
+                assertThat("value="+value,sut.contains(value),is(true));
+            }
+        }
     }
 }
